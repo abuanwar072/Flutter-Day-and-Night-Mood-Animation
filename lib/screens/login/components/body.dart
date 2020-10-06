e3f58dcaae6512c22990e82bd3dd6e8d498d53c3
Abu Anwar <abuanwar072@gmail.com>
2020-10-06 20:52:07 +0600

    starting project

--- lib/screens/login/components/body.dart
+++ lib/screens/login/components/body.dart
@@ -1,11 +1,9 @@
-import 'dart:async';
-
 import 'package:animation/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'land.dart';
 import 'rounded_text_field.dart';
-import 'sun.dart';
 import 'tabs.dart';
 
 class Body extends StatefulWidget {
@@ -14,70 +12,30 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  bool isFullSun = false;
-  bool isDayMood = true;
-  Duration _duration = Duration(seconds: 1);
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration(seconds: 1), () {
-      setState(() {
-        isFullSun = true;
-      });
-    });
-  }
-
-  void changeMood(int activeTabNum) {
-    if (activeTabNum == 0) {
-      setState(() {
-        isDayMood = true;
-      });
-      Future.delayed(Duration(milliseconds: 300), () {
-        setState(() {
-          isFullSun = true;
-        });
-      });
-    } else {
-      setState(() {
-        isFullSun = false;
-      });
-      Future.delayed(Duration(milliseconds: 300), () {
-        setState(() {
-          isDayMood = false;
-        });
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     List<Color> lightBgColors = [
       Color(0xFF8C2480),
       Color(0xFFCE587D),
       Color(0xFFFF9485),
-      if (isFullSun) Color(0xFFFF9D80),
     ];
-    var darkBgColors = [
-      Color(0xFF0D1441),
-      Color(0xFF283584),
-      Color(0xFF376AB2),
-    ];
-    return AnimatedContainer(
-      duration: _duration,
-      curve: Curves.easeInOut,
+
+    return Container(
       width: double.infinity,
       height: SizeConfig.screenHeight,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: isDayMood ? lightBgColors : darkBgColors,
         ),
       ),
       child: Stack(
         children: [
-          Sun(duration: _duration, isFullSun: isFullSun),
+          Positioned(
+            left: getProportionateScreenWidth(30),
+            bottom: getProportionateScreenWidth(-45),
+            child: SvgPicture.asset("assets/icons/Sun.svg"),
+          ),
           Land(),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
@@ -87,9 +45,7 @@ class _BodyState extends State<Body> {
                 children: [
                   VerticalSpacing(of: 50),
                   Tabs(
-                    press: (value) {
-                      changeMood(value);
-                    },
+                    press: (value) {},
                   ),
                   VerticalSpacing(),
                   Text(
